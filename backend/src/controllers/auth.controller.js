c9b483028f68b51c6f6e10d86c29726f0e00a41e
ajayvagh <ajayvaghav8@gmail.com>
2025-03-13 16:49:10 +0530

    login logout done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -48,10 +48,48 @@ export const signup = async (req, res) => {
     }
 };
 
-export const login = (req, res) => {
-    res.send("login route");
+export const login = async (req, res) => {
+    const { email, password } = req.body
+    
+    try {
+        const user = await User.findOne({email})
+
+        if(!user) {
+            return res.status(400).json({message : "User Not Found"});
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)         //decrypt password adn check
+        if (!isPasswordCorrect) {
+            return res.status(400).json({message : "User Not Found"});
+        }
+
+        generateToken(user._id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        })
+
+    } catch (error) {
+        console.log("Error in Login Container", error.message)
+        res.status(500).json({message: "Internal Server ERROR"});
+    }
 }
 
 export const logout = (req, res) => {
-    res.send("logout route");
+
+    try {
+        res.cookie("jwt", "", {maxAge:0})
+        res.status(200).json({message : "Logged Out succesfully"})
+
+    } catch (error) {
+        console.log("Error in Logout Controller", error.message)
+        res.status(500).json({ message : "Internal server ERROR"})
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    
 }
