6c5f4df91273d9ee25a7be1bcdc8459966d3e12a
ajayvagh <ajayvaghav8@gmail.com>
2025-03-13 17:57:00 +0530

    Message router

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js"
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config()
  const app = express();
@@ -15,6 +16,7 @@ app.use(express.json());      // allows extract data from json body
 app.use(cookieParser());      // allow to parse cookie
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, () => {
    console.log("server running on PORT: "+ PORT);
