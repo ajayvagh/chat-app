ccdad1d0410884267ce5f32b654f238863b66a8d
ajayvagh <ajayvaghav8@gmail.com>
2025-03-14 18:08:49 +0530

    Chat container completed

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,67 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+import { useAuthStore } from "./useAuthStore";
+
+
+export const useChatStore = create((set, get) => ({
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+
+
+    getUsers: async () => {
+        set({ isUsersLoading: true });
+        try {
+            const res = await axiosInstance.get("/messages/users");
+            set({ users: res.data });
+        } catch (error) {
+            if (error.response && error.response.data && error.response.data.message) {
+                toast.error(error.response.data.message);
+            } else {
+                toast.error("An unexpected error occurred. Please try again.");
+            }
+        } finally {
+            set({ isUsersLoading: false });
+        }
+    },
+
+    getMessages: async (userId) => {
+        set({ isMessagesLoading: true });
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`);
+            set({ messages: res.data });
+        } catch (error) {
+            if (error.response && error.response.data && error.response.data.message) {
+                toast.error(error.response.data.message);
+            } else {
+                toast.error("Failed to fetch messages. Please try again.");
+            }
+        } finally {
+            set({ isMessagesLoading: false });
+        }
+    },
+
+    sendMessage: async (messageData) => {
+        const { selectedUser, messages } = get();
+        try {
+            const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+            set({ messages: [...messages, res.data] });
+        } catch (error) {
+            if (error.response && error.response.data && error.response.data.message) {
+                toast.error(error.response.data.message);
+            } else {
+                toast.error("Failed to send message. Please try again.");
+            }
+        }
+    },
+    
+
+
+    // Optimization pending
+    setSelectedUser: (selectedUser) => set({ selectedUser }),
+    
+    
+}))
